487732f7edfc5083a83e321e193c255139c5d18d
heliostate <heliostate@protonmail.com>
2021-04-07 20:49:42 -0600

    getting basic stats from all fixtures

--- stats.rb
+++ stats.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'nokogiri'
 
 ANALYZED_DIRECTORY = ARGV[0]
 
@@ -22,7 +23,49 @@ def for_each_directory &block
   end
 end
 
+def boilerplate_fixture?(description_xml)
+  parsed_description = Nokogiri::XML(description_xml)
+
+  # nokogiri finds several XML::Text nodes with newlines as the contained text, so we seslect only elements
+  modes = parsed_description.xpath('//GDTF/FixtureType/DMXModes').children.select{ |mode| mode.is_a? Nokogiri::XML::Element}
+  if modes.length > 1
+    false
+  elsif modes.length == 0
+    true
+  else
+    mode = modes.first
+    channels =  mode.at('DMXChannels').children.select { |mode| mode.is_a? Nokogiri::XML::Element }
+    (mode.attributes["Name"].value == "DMX Mode") && (channels.length == 1)
+  end
+end
+
+total_gobo_count = 0
+fixtures_with_gobos_count = 0
+fixtures_with_models = 0
+boilerplate_fixture_count = 0
+
 for_each_directory do |gdtf_path|
   # puts gdtf_path
-  puts Dir.glob("#{gdtf_path}/*").inspect
+  directory_contents = Dir.glob("#{gdtf_path}/*")
+  directory_contents_names = directory_contents.flat_map{ |path| path.split('/').last }
+
+  if directory_contents_names.include? "wheels"
+    fixture_gobo_count = Dir.glob("#{gdtf_path}/wheels/*").size
+    total_gobo_count += fixture_gobo_count
+    fixtures_with_gobos_count += 1
+  end
+
+  if directory_contents_names.include? "models"
+    fixtures_with_models += 1
+  end
+
+  fixture_description = File.open("#{gdtf_path}/description.xml").read
+  if boilerplate_fixture?(fixture_description)
+    boilerplate_fixture_count += 1
+  end
 end
+
+puts "fixtures with gobos: #{fixtures_with_gobos_count}"
+puts "total gobo files: #{total_gobo_count}"
+puts "fixtures with models: #{fixtures_with_models}"
+puts "boilerplate fixtures: #{boilerplate_fixture_count}"
